69bc31dca188ba293b4ffef9b652a97192d24c22
S <844234@cognizant.com>
2020-11-23 13:39:34 +0530

    nested tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaxComponent } from './tax/tax.component';
+import { NotesComponent } from './notes/notes.component';
+import { DocumentsComponent } from './documents/documents.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'tax',component:TaxComponent},
+  {path:'notes',component:NotesComponent},
+  {path:'documents',component:DocumentsComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ BrowserAnimationsModule} from '@angular/platform-browser/animations';
+                       
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatTabsModule} from '@angular/material/tabs';
+
+import { TaxComponent } from './tax/tax.component';
+import { NotesComponent } from './notes/notes.component';
+import { DocumentsComponent } from './documents/documents.component';
+//import { TabComponent } from './tab/tab.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaxComponent,
+    NotesComponent,
+    DocumentsComponent,
+   
+  
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule
+  
+    //MatButtonModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
